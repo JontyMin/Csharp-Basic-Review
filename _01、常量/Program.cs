d5f309bbcdd89d6e95349ae88788567c6883a281
JontyWang <501211312@qq.com>
2020-07-06 20:35:22 +0800

    添加项目文件。

---
+++ _01、常量/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_常量
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 10;
+            number = 18;// 变量可以被重新复制
+
+            // 1、常量 const
+            const int age = 18;
+            //age = 20; 常量不能被重新赋值
+
+            // 什么时候会用到常量？
+
+        }
+    }
+}
