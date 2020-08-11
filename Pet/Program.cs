991312b8b312e9c42493cfa479c9afe0be2295e1
JontyWang <501211312@qq.com>
2020-08-11 09:14:24 +0800

    继承

---
+++ Pet/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Pet
+{
+    /*
+     * Object是所有类的共同基类
+     * 它是唯一的非派生类
+     * 是继承层次的基础
+     *
+     * 单一继承
+     */
+
+    /// <summary>
+    /// 宠物父类
+    /// </summary>
+    public class Pet
+    {
+        public string name;
+
+        public void PrintName()
+        {
+            Console.WriteLine($"Pet's name is {name}");
+        }
+    }
+
+    /// <summary>
+    ///  狗类 继承自父类Pet
+    /// </summary>
+    public class Dog : Pet
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            dog.name = "jack";
+            dog.PrintName();
+        }
+    }
+}
