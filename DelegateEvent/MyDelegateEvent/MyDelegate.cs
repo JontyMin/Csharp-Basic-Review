5c9e7995ba00328026ffe4ca415e305f7d9eb052
JontyWang <501211312@qq.com>
2020-09-07 19:24:57 +0800

    delegate-1

---
+++ DelegateEvent/MyDelegateEvent/MyDelegate.cs
@@ -0,0 +1,19 @@
+﻿namespace MyDelegateEvent
+{
+    public class MyDelegate
+    {
+        /// <summary>
+        /// 无参数无返回值
+        /// </summary>
+        public delegate void NoReturnNoPara();
+
+        /// <summary>
+        /// 有参数无返回值
+        /// </summary>
+        public delegate void NoReturnWithPara(int x,int y);
+
+        public delegate int WithReturnNoPara();
+
+        public delegate string WithReturnWithPara(out int x, ref int y);
+    }
+}
